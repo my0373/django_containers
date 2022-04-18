99f3eebc3a75c2126943180846b5b5a24c267f66
Matt York <my0373@gmail.com>
2022-04-18 21:13:41 +0100

    Added a simple member model

--- camerawl/members/models.py
+++ camerawl/members/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Member(models.Model):
+    """
+
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    nickname = models.CharField(max_length=100)
+    is_parent = models.BooleanField(default=False)
